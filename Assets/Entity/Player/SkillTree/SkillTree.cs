87a86a908b1667fe9c419af959ce7c0ac2441fb8
Loh10 <naturellement.toi@gmail.com>
2025-03-17 14:34:08 +0100

    Deplacement Leveling

--- Assets/Entity/Player/SkillTree/SkillTree.cs
+++ Assets/Entity/Player/SkillTree/SkillTree.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class SkillTree : MonoBehaviour
 {
     public UIWindow skillTreeWindow;
+    private Vector3 _initialMousePosition;
+    private bool _isDragging;
 
     public void PlayerOpenSkillTreeInput(InputAction.CallbackContext cxt)
     {
@@ -24,5 +28,36 @@ public class SkillTree : MonoBehaviour
                 nodeComp.UnlockButton();
             }
         }
+
+        skillTreeWindow.transform.GetComponentInChildren<RectMask2D>().transform.GetChild(0).position = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (skillTreeWindow.CheckOpened())
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                _isDragging = true;
+                _initialMousePosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                _isDragging = false;
+            }
+
+            if (_isDragging)
+            {
+                MoveWindow();
+            }
+        }
+    }
+
+    private void MoveWindow()
+    {
+        Vector3 currentMousePosition = Input.mousePosition;
+        Vector3 difference = currentMousePosition - _initialMousePosition;
+        skillTreeWindow.transform.GetComponentInChildren<RectMask2D>().transform.GetChild(0).position += difference;
+        _initialMousePosition = currentMousePosition;
     }
 }
